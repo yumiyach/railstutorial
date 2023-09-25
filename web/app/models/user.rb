fdb1fef18ea8d9f6050c55a384a0806c6a6c3bc8
弓矢 <yumiyachizuru@gmail.com>
2023-09-25 13:21:11 +0900

    Finish user edit, update, index, and destroy actions

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence:   true, length: { maximum: 255 },
-                    format:     { with: VALID_EMAIL_REGEX },
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
@@ -21,13 +21,13 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
-  # 永続的セッションのためにユーザーをデータベースに記憶する
+  # 永続化セッションのためにユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
     remember_digest
   end
-  
+
   # セッションハイジャック防止のためにセッショントークンを返す
   # この記憶ダイジェストを再利用しているのは単に利便性のため
   def session_token
